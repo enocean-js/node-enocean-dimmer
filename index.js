8424047a1cc71ebe1c5ac27bd8923576f6d95619
Holger Will <h.will@klimapartner.de>
2015-10-25 09:19:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ module.exports        = function ( app , id ) {
 	this.head     = "55000a0701eb";
 	this.address  = ( parseInt( app.base , 16 ) + parseInt( id ) ).toString(16)
 	this.speed    = "01"
-	console.log(this.address)
 	this.setValue = function( val ) {
 		var value = app.pad(parseInt(val).toString(16),2)
 		var b1    = "a502" + value + this.speed + "09" + this.address + "3001ffffffffff00"
