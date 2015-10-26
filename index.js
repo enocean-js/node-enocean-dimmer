9d0174cf2985cdce0b83ed921f3e42acf1b11a57
Holger Will <h.will@klimapartner.de>
2015-10-26 12:22:19 +0100

    0xa5 does indeed work. rolling back to the old code

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ module.exports        = function ( app , id ) {
 	this.speed    = "01"
 	this.setValue = function( val ) {
 		var value = app.pad(parseInt(val).toString(16),2)
-		var b1    = "0702" + value + this.speed + "09" + this.address + "3001ffffffffff00"
+		var b1    = "a502" + value + this.speed + "09" + this.address + "3001ffffffffff00"
 		b1       += app.pad( app.crc( new Buffer( b1 , "hex" ) ).toString( 16 ) , 2 )
 		app.send( this.head + b1 )
 	}
 	this.teach    = function () {
-		var b1    = "0702000000" + this.address + "3001ffffffffff00"
+		var b1    = "a502000000" + this.address + "3001ffffffffff00"
 		b1       += app.pad( app.crc( new Buffer( b1 , "hex" ) ).toString( 16 ) , 2 )
 		app.send( this.head + b1 )
 	}
 	this.off=function(){
-		var b1    = "0702000008" + this.address + "3001ffffffffff00"
+		var b1    = "a502000008" + this.address + "3001ffffffffff00"
 		b1       += app.pad( app.crc( new Buffer(b1,"hex") ).toString( 16 ) , 2 )
 		app.send( this.head + b1 )
 	}
