4c2ebf2d80902d5fcf6283eddc0945c352589c5c
Holger Will <h.will@klimapartner.de>
2016-08-17 09:24:20 +0200

    switch to sendAsync()

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
+var async = require('asyncawait/async');
+var await = require('asyncawait/await');
 module.exports        = function ( app , id ) {
 	this.head     = "55000a0701eb";
 	this.address  = ( parseInt( app.base , 16 ) + parseInt( id ) ).toString(16)
 	this.speed    = "01"
-	this.setValue = function( val ) {
+	this.setValue = async (function( val ) {
 		var value = app.pad(parseInt(val).toString(16),2)
 		var b1    = "a502" + value + this.speed + "09" + this.address + "3001ffffffffff00"
 		b1       += app.pad( app.crc( new Buffer( b1 , "hex" ) ).toString( 16 ) , 2 )
-		app.send( this.head + b1 )
-	}
-	this.teach    = function () {
+		await (app.sendAsync( this.head + b1 ))
+	})
+	this.teach    = async (function () {
 		var b1    = "a502000000" + this.address + "3001ffffffffff00"
 		b1       += app.pad( app.crc( new Buffer( b1 , "hex" ) ).toString( 16 ) , 2 )
-		app.send( this.head + b1 )
-	}
-	this.off=function(){
+		await (app.sendAsync( this.head + b1 ))
+	})
+	this.off=async (function(){
 		var b1    = "a502000008" + this.address + "3001ffffffffff00"
 		b1       += app.pad( app.crc( new Buffer(b1,"hex") ).toString( 16 ) , 2 )
-		app.send( this.head + b1 )
-	}
+		await(app.sendAsync( this.head + b1 ))
+	})
 }
