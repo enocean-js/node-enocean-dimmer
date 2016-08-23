86722cd24a61d70c99e05fd1cab1ec3316755589
Holger Will <h.will@klimapartner.de>
2016-08-23 09:34:03 +0200

    clamp values to Range 0-100

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ module.exports        = function ( app , id ) {
 	this.address  = ( parseInt( app.base , 16 ) + parseInt( id ) ).toString(16)
 	this.speed    = "01"
 	this.setValue = async (function( val ) {
+		if(val>100) val=100
+		if(val<0) val=0
 		var value = app.pad(parseInt(val).toString(16),2)
+		if(val<10){
+			await(this.off)
+			return
+		}
 		var b1    = "a502" + value + this.speed + "09" + this.address + "3001ffffffffff00"
 		b1       += app.pad( app.crc( new Buffer( b1 , "hex" ) ).toString( 16 ) , 2 )
 		await (app.sendAsync( this.head + b1 ))
